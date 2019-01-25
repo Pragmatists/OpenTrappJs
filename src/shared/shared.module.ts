2e0609421c7108ea9a70395c30932e36a7870311
Hubert Legęć <hubert.legec@gmail.com>
2019-01-25 22:53:26 +0100

    Remove unused ParseDatePipe

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ParseDatePipe } from './parse-date.pipe';
 import { ConfigService } from './config.service';
 import { RolesGuard } from './roles.guard';
 import { BcryptService } from './bcrypt.service';
 
 @Module({
-  providers: [ParseDatePipe, ConfigService, RolesGuard, BcryptService],
-  exports: [ParseDatePipe, ConfigService, RolesGuard, BcryptService]
+  providers: [ConfigService, RolesGuard, BcryptService],
+  exports: [ConfigService, RolesGuard, BcryptService]
 })
 export class SharedModule {
 }
